6f8dc3b771a433a3a9c8298a31791882df3f53cc
Marlon Monroy <saito.sv.h@gmail.com>
2022-09-14 15:51:05 -0700

    base

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -1,10 +1,13 @@
-package api
+package middleware
 
 import (
 	"net/http"
+	"os"
 	"regexp"
 
+	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
 )
 
 func allowOrigin(origin string) (bool, error) {
@@ -12,13 +15,32 @@ func allowOrigin(origin string) (bool, error) {
 	return regexp.MatchString(`*`, origin)
 }
 
-type MiddlewareConfig struct {
-	any any
-}
-
-func StringMiddleware(config MiddlewareConfig) {
-	echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
+func CORS() echo.MiddlewareFunc {
+	return echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
 		AllowOriginFunc: allowOrigin,
 		AllowMethods:    []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	})
+}
+
+func Recover() echo.MiddlewareFunc {
+	return echoMiddleware.Recover()
+}
+
+func Logger() echo.MiddlewareFunc {
+	logger := zerolog.New(os.Stdout)
+	return echoMiddleware.RequestLoggerWithConfig(echoMiddleware.RequestLoggerConfig{
+		LogURI:       true,
+		LogStatus:    true,
+		LogRequestID: true,
+		LogLatency:   true,
+		LogValuesFunc: func(c echo.Context, v echoMiddleware.RequestLoggerValues) error {
+			logger.Info().
+				Str("URI", v.URI).
+				Int("Status", v.Status).
+				Str("RequestID", v.RequestID).
+				Dur("latency", v.Latency).
+				Msg("request")
+			return nil
+		},
 	})
 }
