cfa750841ead3ea58260367d77377b78a41830e1
Auroter <auroter34@gmail.com>
2022-09-17 15:06:32 -0700

    getting started on porting Quote

---
+++ pkg/internal/common/cost.go
@@ -0,0 +1,13 @@
+package common
+
+func NativeTokenBuffer(chainID uint32) float32 {
+	return 0.05
+}
+
+func GasBuffer(chainID uint32) float32 {
+	return 0.05
+}
+
+func TokenBuffer(token string) float32 {
+	return 0.05
+}
