d106d07faf38ff953534d180ff683752e5269e5d
Ocasta <ocasta181@gmail.com>
2022-09-28 15:05:52 -0700

    update based on unit21 responses

--- migrations/0003_contact-platform_device-instrument_transaction.sql
+++ migrations/0003_contact-platform_device-instrument_transaction.sql
@@ -36,38 +36,6 @@ CREATE OR REPLACE TRIGGER update_device_instrument_updated_at
 EXECUTE PROCEDURE update_updated_at_column();
 
 -------------------------------------------------------------------------
--- BLOCKCHAIN_TX --------------------------------------------------------
-CREATE TABLE blockchain_tx (
-  id UUID PRIMARY KEY NOT NULL DEFAULT UUID_GENERATE_V4(),
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-  updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-  timestamp TIMESTAMP WITH TIME ZONE,
-  status TEXT DEFAULT '', --enum
-  tags JSONB DEFAULT '[]'::JSONB,
-  transaction_hash TEXT DEFAULT '',
-  network_id UUID NOT NULL REFERENCES network (id),
-  network_fee BIGINT DEFAULT 0,
-  parameters JSONB DEFAULT '{}'::JSONB,
-  contract_abi JSONB DEFAULT '{}'::JSONB,
-  sender_amount BIGINT DEFAULT 0,
-  sender_asset UUID REFERENCES asset (id),
-  sender_value BIGINT DEFAULT 0,
-  sender_user_id UUID NOT NULL REFERENCES string_user (id),
-  sender_instrument_id UUID NOT NULL REFERENCES instrument (id),
-  reciever_amount BIGINT DEFAULT 0,
-  reciever_asset UUID NOT NULL REFERENCES asset (id),
-  reciever_value BIGINT DEFAULT 0,
-  reciever_user_id UUID NOT NULL REFERENCES string_user (id),
-  reciever_instrument_id UUID NOT NULL REFERENCES instrument (id)
-);
-
-CREATE OR REPLACE TRIGGER update_blockchain_tx_updated_at
-    BEFORE UPDATE
-    ON blockchain_tx
-    FOR EACH ROW
-EXECUTE PROCEDURE update_updated_at_column();
-
--------------------------------------------------------------------------
 -- TRANSACTION ----------------------------------------------------------
 CREATE TABLE transaction (
   id UUID PRIMARY KEY NOT NULL DEFAULT UUID_GENERATE_V4(),
@@ -80,17 +48,31 @@ CREATE TABLE transaction (
   device_id UUID REFERENCES device (id),
   ip_address TEXT DEFAULT '',
   platform_id UUID REFERENCES platform (id),
-  sender_amount BIGINT DEFAULT 0, -- will need to define the base for USD and other fiat currencies
-  sender_asset UUID REFERENCES asset (id),
-  sender_value BIGINT DEFAULT 0, -- the relative quantity in USD at the time of the transaction
-  sender_user_id UUID NOT NULL REFERENCES string_user (id),
-  sender_instrument_id UUID NOT NULL REFERENCES instrument (id),
-  reciever_amount BIGINT DEFAULT 0,
-  reciever_asset UUID NOT NULL REFERENCES asset (id),
-  reciever_value BIGINT DEFAULT 0,
-  reciever_user_id UUID NOT NULL REFERENCES string_user (id),
-  reciever_instrument_id UUID NOT NULL REFERENCES instrument (id),
-  blockchain_tx_id UUID NOT NULL REFERENCES blockchain_tx (id),
+  transaction_hash TEXT DEFAULT '',
+  network_id UUID NOT NULL REFERENCES network (id),
+  network_fee BIGINT DEFAULT 0,
+  parameters JSONB DEFAULT '{}'::JSONB,
+  contract_abi JSONB DEFAULT '{}'::JSONB,
+  origin_amount BIGINT DEFAULT 0, -- will need to define the base for USD and other fiat currencies
+  origin_asset UUID REFERENCES asset (id),
+  origin_value BIGINT DEFAULT 0, -- the relative quantity in USD at the time of the transaction
+  origin_user_id UUID NOT NULL REFERENCES string_user (id),
+  origin_instrument_id UUID NOT NULL REFERENCES instrument (id),
+  reciept_amount BIGINT DEFAULT 0,
+  reciept_asset UUID REFERENCES asset (id),
+  reciept_value BIGINT DEFAULT 0,
+  reciept_user_id UUID NOT NULL REFERENCES string_user (id),
+  reciept_instrument_id UUID NOT NULL REFERENCES instrument (id),
+  response_amount BIGINT DEFAULT 0,
+  response_asset UUID NOT NULL REFERENCES asset (id),
+  response_value BIGINT DEFAULT 0,
+  response_user_id UUID NOT NULL REFERENCES string_user (id),
+  response_instrument_id UUID NOT NULL REFERENCES instrument (id),
+  destination_amount BIGINT DEFAULT 0,
+  destination_asset UUID NOT NULL REFERENCES asset (id),
+  destination_value BIGINT DEFAULT 0,
+  destination_user_id UUID NOT NULL REFERENCES string_user (id),
+  destination_instrument_id UUID NOT NULL REFERENCES instrument (id),
   processing_fee BIGINT DEFAULT 0,
   processing_fee_asset UUID REFERENCES asset (id),
   string_fee BIGINT DEFAULT 0 -- // always USD?
@@ -109,11 +91,6 @@ EXECUTE PROCEDURE update_updated_at_column();
 -- TRANSACTION ----------------------------------------------------------
 DROP TRIGGER IF EXISTS update_transaction_updated_at ON transaction;
 DROP TABLE transaction;
-
--------------------------------------------------------------------------
--- BLOCKCHAIN_TX --------------------------------------------------------
-DROP TRIGGER IF EXISTS update_blockchain_tx_updated_at ON blockchain_tx;
-DROP TABLE blockchain_tx;
 
 -------------------------------------------------------------------------
 -- DEVICE_INSTRUMENT ----------------------------------------------------
