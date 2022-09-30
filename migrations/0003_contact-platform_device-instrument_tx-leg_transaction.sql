bf712cc7d26a59f697cbcb065a55c44760995f3d
Ocasta <ocasta181@gmail.com>
2022-09-29 18:25:41 -0700

    break out transaction leg (tx_leg) in order to isolate update hotspot and allow for time-series capture

--- migrations/0003_contact-platform_device-instrument_tx-leg_transaction.sql
+++ migrations/0003_contact-platform_device-instrument_tx-leg_transaction.sql
@@ -36,6 +36,26 @@ CREATE OR REPLACE TRIGGER update_device_instrument_updated_at
 EXECUTE PROCEDURE update_updated_at_column();
 
 -------------------------------------------------------------------------
+-- TX_LEG ----------------------------------------------------
+CREATE TABLE tx_leg (
+  id UUID PRIMARY KEY NOT NULL DEFAULT UUID_GENERATE_V4(),
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  timestamp TIMESTAMP WITH TIME ZONE,
+  amount BIGINT DEFAULT 0, -- will need to define the base for USD and other fiat currencies
+  asset UUID REFERENCES asset (id),
+  value BIGINT DEFAULT 0, -- the relative quantity in USD at the time of the transaction
+  user_id UUID NOT NULL REFERENCES string_user (id),
+  instrument_id UUID NOT NULL REFERENCES instrument (id)
+);
+
+CREATE OR REPLACE TRIGGER update_tx_leg_updated_at
+    BEFORE UPDATE
+    ON tx_leg
+    FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+-------------------------------------------------------------------------
 -- TRANSACTION ----------------------------------------------------------
 CREATE TABLE transaction (
   id UUID PRIMARY KEY NOT NULL DEFAULT UUID_GENERATE_V4(),
@@ -51,28 +71,12 @@ CREATE TABLE transaction (
   transaction_hash TEXT DEFAULT '',
   network_id UUID NOT NULL REFERENCES network (id),
   network_fee BIGINT DEFAULT 0,
-  parameters JSONB DEFAULT '{}'::JSONB,
+  parameters JSONB DEFAULT '{}'::JSONB, 
   contract_abi JSONB DEFAULT '{}'::JSONB,
-  origin_amount BIGINT DEFAULT 0, -- will need to define the base for USD and other fiat currencies
-  origin_asset UUID REFERENCES asset (id),
-  origin_value BIGINT DEFAULT 0, -- the relative quantity in USD at the time of the transaction
-  origin_user_id UUID NOT NULL REFERENCES string_user (id),
-  origin_instrument_id UUID NOT NULL REFERENCES instrument (id),
-  receipt_amount BIGINT DEFAULT 0,
-  receipt_asset UUID REFERENCES asset (id),
-  receipt_value BIGINT DEFAULT 0,
-  receipt_user_id UUID NOT NULL REFERENCES string_user (id),
-  receipt_instrument_id UUID NOT NULL REFERENCES instrument (id),
-  response_amount BIGINT DEFAULT 0,
-  response_asset UUID NOT NULL REFERENCES asset (id),
-  response_value BIGINT DEFAULT 0,
-  response_user_id UUID NOT NULL REFERENCES string_user (id),
-  response_instrument_id UUID NOT NULL REFERENCES instrument (id),
-  destination_amount BIGINT DEFAULT 0,
-  destination_asset UUID NOT NULL REFERENCES asset (id),
-  destination_value BIGINT DEFAULT 0,
-  destination_user_id UUID NOT NULL REFERENCES string_user (id),
-  destination_instrument_id UUID NOT NULL REFERENCES instrument (id),
+  origin_tx_leg_id UUID NOT NULL REFERENCES tx_leg (id),
+  receipt_tx_leg_id UUID NOT NULL REFERENCES tx_leg (id),
+  response_tx_leg_id UUID NOT NULL REFERENCES tx_leg (id),
+  destination_tx_leg_id UUID NOT NULL REFERENCES tx_leg (id),
   processing_fee BIGINT DEFAULT 0,
   processing_fee_asset UUID REFERENCES asset (id),
   string_fee BIGINT DEFAULT 0 -- // always USD?
@@ -91,6 +95,11 @@ EXECUTE PROCEDURE update_updated_at_column();
 -- TRANSACTION ----------------------------------------------------------
 DROP TRIGGER IF EXISTS update_transaction_updated_at ON transaction;
 DROP TABLE transaction;
+
+-------------------------------------------------------------------------
+-- TX_LEG ----------------------------------------------------
+DROP TRIGGER IF EXISTS update_tx_leg_updated_at ON tx_leg;
+DROP TABLE tx_leg;
 
 -------------------------------------------------------------------------
 -- DEVICE_INSTRUMENT ----------------------------------------------------
