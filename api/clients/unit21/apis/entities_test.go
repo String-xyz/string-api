47fe1ff46293101085f9a5ef011d30caba3deb97
Kelly Whiting <kwhiting@gmail.com>
2022-10-10 00:47:59 -0700

    add unit21 create entity

---
+++ api/clients/unit21/apis/entities_test.go
@@ -0,0 +1,11 @@
+package unit21
+
+import (
+	"testing"
+)
+
+// calls entities.CreateEntity()
+// todo: mock call to client once it's manually tested
+func TestCreateEntity(t *testing.T) {
+	return true
+}
