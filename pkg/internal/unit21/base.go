204ecebc6a9bc2684201b28282cfc4d0fafa4b90
Ocasta <ocasta181@gmail.com>
2022-10-19 15:19:08 -0500

    AddInstruments working as expected, CreateEntity test requires a mock.ExpectQuery it seems

--- pkg/internal/unit21/base.go
+++ pkg/internal/unit21/base.go
@@ -27,7 +27,7 @@ import (
 // jsonBody = MapUserToEntity(user)
 func create(datatype string, jsonBody any) (body []byte, err error) {
 	apiKey := os.Getenv("UNIT21_API_KEY")
-	url := os.Getenv("UNIT21_URL") + "/" + datatype + "/create"
+	url := os.Getenv("UNIT21_URL") + datatype + "/create"
 
 	reqBodyBytes, err := json.Marshal(jsonBody)
 	if err != nil {
--- pkg/internal/unit21/entities_test.go
+++ pkg/internal/unit21/entities_test.go
@@ -13,27 +13,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// StringData {
-// 	id            string
-// 	tags          map[string]string
-// 	partnerName   string
-// 	status        string
-// 	createdAt     int
-// 	firstName     string
-// 	middleName    string
-// 	lastName      string
-// 	emails        []string
-// 	phones        []string
-// 	ipAddresses   []string
-// 	fingerprints  []string
-// }
-
 func TestCreateEntity(t *testing.T) {
 	err := godotenv.Load("../../../.env")
 	assert.NoError(t, err)
 
-	id := uuid.NewString()
-	db, _, err := sqlmock.New()
+	entityId := uuid.NewString()
+	db, mock, err := sqlmock.New()
 	sqlxDB := sqlx.NewDb(db, "sqlmock")
 	if err != nil {
 		t.Fatalf("error %s was not expected when opening stub db", err)
@@ -41,7 +26,7 @@ func TestCreateEntity(t *testing.T) {
 	defer db.Close()
 
 	user := model.User{
-		ID:            id,
+		ID:            entityId,
 		CreatedAt:     time.Now(),
 		UpdatedAt:     time.Now(),
 		DeactivatedAt: nil,
@@ -53,6 +38,9 @@ func TestCreateEntity(t *testing.T) {
 		LastName:      "User",
 	}
 
+	// "SELECT * FROM %s WHERE user_id = $1 LIMIT $2 OFFSET $3", b.table), userID, limit, offset)
+	mock.ExpectQuery("SELECT * FROM (.+) WHERE user_id = (.+) LIMIT (.+) OFFSET (.+)")
+
 	// Dependent on Device and Instrument Repos being created
 	userRepo := repository.NewUser(sqlxDB)
 	// deviceRepo := repository.NewDevice(sqlxDB)
@@ -63,6 +51,41 @@ func TestCreateEntity(t *testing.T) {
 	u21Entity := newEntity(userRepo, contactRepo)
 
 	_, err = u21Entity.Create(user)
+	assert.NoError(t, err)
+
+	//validate response from Unit21
+	//check Unit21 dashboard for new entity added
+	// todo: mock call to client once it's manually tested
+}
+
+func TestAddInstruments(t *testing.T) {
+	err := godotenv.Load("../../../.env")
+	assert.NoError(t, err)
+
+	entityId := "44142758-f015-4f79-a004-e554b0641480" //previous created test user
+	var instrumentIds []string
+	db, _, err := sqlmock.New()
+	sqlxDB := sqlx.NewDb(db, "sqlmock")
+	if err != nil {
+		t.Fatalf("error %s was not expected when opening stub db", err)
+	}
+	defer db.Close()
+
+	// mock new instrumentIds
+	for i := 1; i <= 10; i++ {
+		instrumentIds = append(instrumentIds, uuid.NewString())
+	}
+
+	// Dependent on Device and Instrument Repos being created
+	userRepo := repository.NewUser(sqlxDB)
+	// deviceRepo := repository.NewDevice(sqlxDB)
+	contactRepo := repository.NewUserContact(sqlxDB)
+	// instrumentRepo := repository.NewInstrument(sqlxDB)
+
+	// u21Entity := newEntity(userRepo, deviceRepo, contactRepo, instrumentRepo)
+	u21Entity := newEntity(userRepo, contactRepo)
+
+	err = u21Entity.AddInstruments(entityId, instrumentIds)
 	assert.NoError(t, err)
 
 	//validate response from Unit21
