e783dceb937babfbe631e7735fc0a15b76ab5e79
Auroter <auroter34@gmail.com>
2022-11-01 15:26:15 -0700

    added StringArray

--- pkg/model/entity.go
+++ pkg/model/entity.go
@@ -4,8 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"time"
-
-	"github.com/jmoiron/sqlx/types"
 )
 
 // See STRING_USER in Migrations 0001
@@ -63,17 +61,17 @@ type Asset struct {
 
 // See DEVICE in Migrations 0002
 type Device struct {
-	ID            string         `json:"id" db:"id"`
-	CreatedAt     time.Time      `json:"createdAt" db:"created_at"`
-	UpdatedAt     time.Time      `json:"updatedAt" db:"updated_at"`
-	LastUsedAt    time.Time      `json:"lastUsedAt" db:"last_used_at"`
-	ValidatedAt   time.Time      `json:"validatedAt" db:"validated_at"`
-	DeactivatedAt time.Time      `json:"deactivatedAt" db:"deactivated_at"`
-	Type          string         `json:"type" db:"type"`
-	Description   string         `json:"description" db:"description"`
-	Fingerprint   string         `json:"fingerprint" db:"fingerprint"`
-	IpAddresses   types.JSONText `json:"ipAddresses" db:"ip_addresses"`
-	UserID        string         `json:"userId" db:"user_id"`
+	ID            string      `json:"id" db:"id"`
+	CreatedAt     time.Time   `json:"createdAt" db:"created_at"`
+	UpdatedAt     time.Time   `json:"updatedAt" db:"updated_at"`
+	LastUsedAt    time.Time   `json:"lastUsedAt" db:"last_used_at"`
+	ValidatedAt   time.Time   `json:"validatedAt" db:"validated_at"`
+	DeactivatedAt time.Time   `json:"deactivatedAt" db:"deactivated_at"`
+	Type          string      `json:"type" db:"type"`
+	Description   string      `json:"description" db:"description"`
+	Fingerprint   string      `json:"fingerprint" db:"fingerprint"`
+	IpAddresses   StringArray `json:"ipAddresses" db:"ip_addresses"`
+	UserID        string      `json:"userId" db:"user_id"`
 }
 
 // See CONTACT in Migrations 0002
@@ -158,28 +156,28 @@ type TxLeg struct {
 
 // See TRANSACTION in Migrations 0003
 type Transaction struct {
-	ID                 string         `json:"id" db:"id"`
-	CreatedAt          time.Time      `json:"createdAt" db:"created_at"`
-	UpdatedAt          time.Time      `json:"updatedAt" db:"updated_at"`
-	Type               string         `json:"type" db:"type"`
-	Status             string         `json:"status" db:"status"`
-	Tags               StringMap      `json:"tags" db:"tags"` // TODO: Fix this alongside Unit21 integration
-	DeviceID           string         `json:"deviceId" db:"device_id"`
-	IPAddress          string         `json:"ipAddress" db:"ip_address"`
-	PlatformID         string         `json:"platformId" db:"platform_id"`
-	TransactionHash    string         `json:"transactionHash" db:"transaction_hash"`
-	NetworkID          string         `json:"networkId" db:"network_id"`
-	NetworkFee         string         `json:"networkFee" db:"network_fee"`
-	ContractParams     types.JSONText `json:"contractParameters" db:"contract_params"`
-	ContractFunc       string         `json:"contractFunc" db:"contract_func"`
-	TransactionAmount  string         `json:"transactionAmount" db:"transaction_amount"`
-	OriginTXLegID      string         `json:"originTXLegId" db:"origin_tx_leg_id"`
-	ReceiptTXLegID     string         `json:"receiptTXLegId" db:"receipt_tx_leg_id"`
-	ResponseTXLegID    string         `json:"responseTXLegId" db:"response_tx_leg_id"`
-	DestinationTXLegID string         `json:"destinationTXLegId" db:"destination_tx_leg_id"`
-	ProcessingFee      string         `json:"processingFee" db:"processing_fee"`
-	ProcessingFeeAsset string         `json:"processingFeeAsset" db:"processing_fee_asset"`
-	StringFee          string         `json:"stringFee" db:"string_fee"`
+	ID                 string      `json:"id" db:"id"`
+	CreatedAt          time.Time   `json:"createdAt" db:"created_at"`
+	UpdatedAt          time.Time   `json:"updatedAt" db:"updated_at"`
+	Type               string      `json:"type" db:"type"`
+	Status             string      `json:"status" db:"status"`
+	Tags               StringMap   `json:"tags" db:"tags"` // TODO: Fix this alongside Unit21 integration
+	DeviceID           string      `json:"deviceId" db:"device_id"`
+	IPAddress          string      `json:"ipAddress" db:"ip_address"`
+	PlatformID         string      `json:"platformId" db:"platform_id"`
+	TransactionHash    string      `json:"transactionHash" db:"transaction_hash"`
+	NetworkID          string      `json:"networkId" db:"network_id"`
+	NetworkFee         string      `json:"networkFee" db:"network_fee"`
+	ContractParams     StringArray `json:"contractParameters" db:"contract_params"`
+	ContractFunc       string      `json:"contractFunc" db:"contract_func"`
+	TransactionAmount  string      `json:"transactionAmount" db:"transaction_amount"`
+	OriginTXLegID      string      `json:"originTXLegId" db:"origin_tx_leg_id"`
+	ReceiptTXLegID     string      `json:"receiptTXLegId" db:"receipt_tx_leg_id"`
+	ResponseTXLegID    string      `json:"responseTXLegId" db:"response_tx_leg_id"`
+	DestinationTXLegID string      `json:"destinationTXLegId" db:"destination_tx_leg_id"`
+	ProcessingFee      string      `json:"processingFee" db:"processing_fee"`
+	ProcessingFeeAsset string      `json:"processingFeeAsset" db:"processing_fee_asset"`
+	StringFee          string      `json:"stringFee" db:"string_fee"`
 }
 
 type AuthStrategy struct {
