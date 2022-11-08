32fee0d9aa673eb934b70887cb903ecddd4d2259
Marlon Monroy <saito.sv.h@gmail.com>
2022-11-08 10:59:05 -0800

    updated stub and removed logs

--- pkg/service/auth_key.go
+++ pkg/service/auth_key.go
@@ -4,7 +4,6 @@ import (
 	"github.com/String-xyz/string-api/pkg/internal/common"
 	"github.com/String-xyz/string-api/pkg/model"
 	"github.com/String-xyz/string-api/pkg/repository"
-	"github.com/rs/zerolog/log"
 )
 
 type APIKeyStrategy interface {
@@ -28,7 +27,6 @@ func (g aPIKeyStrategy) Create() (string, error) {
 }
 
 func (g aPIKeyStrategy) List(limit, offset int, status string) ([]model.AuthStrategy, error) {
-	log.Info().Int("limit", limit).Int("offset", offset).Str("status", status).Msg("List")
 	if status != "" {
 		return g.ListByStatus(limit, offset, status)
 	}
