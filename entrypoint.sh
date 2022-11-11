a76d01a7a6c363e0f2d862a0d7340fadff3c1b0e
Wilfredo Alcala <alcalawilfre@gmail.com>
2022-11-11 16:20:10 -0500

    avoid reseting db on build

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export $(grep -v '^#' .env | xargs)
 # run db migrations
 echo "Running DB Migrations..."
 DB_CONFIG="host=$DB_HOST user=$DB_USERNAME dbname=$DB_NAME sslmode=disable password=$DB_PASSWORD"
-goose postgres "$DB_CONFIG" reset
+# goose postgres "$DB_CONFIG" reset # uncomment to reset db on build
 cd migrations
 goose postgres "$DB_CONFIG" up
 echo '>> Done!'
