3cf4fca5dbeaa38ec7b4886724397ca7732d48ec
Auroter <auroter34@gmail.com>
2022-11-18 16:06:42 -0700

    Added safeguard against errors which do not implement stacktrace (external)

--- pkg/internal/common/json.go
+++ pkg/internal/common/json.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"reflect"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 func GetJson(url string, target interface{}) error {
@@ -17,6 +20,10 @@ func GetJson(url string, target interface{}) error {
 	jsonData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return StringError(err)
+	}
+	targetType := reflect.TypeOf(target)
+	if len(jsonData) != int(targetType.Size()) {
+		return StringError(errors.New("Malformed JSON Response"))
 	}
 	err = json.Unmarshal([]byte(jsonData), target)
 	if err != nil {
