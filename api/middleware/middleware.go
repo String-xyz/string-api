52fc53274b2b763500b4b5d7b6130aebf86e7f19
Wilfredo Alcala <alcalawilfre@gmail.com>
2022-11-29 17:30:06 -0400

    fix TokenLookup value: use comma instead spaces

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -60,7 +60,7 @@ func RequestID() echo.MiddlewareFunc {
 
 func BearerAuth() echo.MiddlewareFunc {
 	config := echoMiddleware.JWTConfig{
-		TokenLookup: "cookie:JWTCookie header:Authorization",
+		TokenLookup: "header:Authorization,cookie:JWTCookie",
 		ParseTokenFunc: func(auth string, c echo.Context) (interface{}, error) {
 			var claims = &service.JWTClaims{}
 			t, err := jwt.ParseWithClaims(auth, claims, func(t *jwt.Token) (interface{}, error) {
