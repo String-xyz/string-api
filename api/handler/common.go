178f8b6997a046bbf0e5c5606f33fbe88bfef4e6
Wilfredo Alcala <alcalawilfre@gmail.com>
2022-11-30 14:37:27 -0400

    Update api/handler/common.go
    Co-authored-by: Auroter <7332587+Auroter@users.noreply.github.com>

--- api/handler/common.go
+++ api/handler/common.go
@@ -40,7 +40,7 @@ func SetJWTCookie(c echo.Context, jwt service.JWT) error {
 	cookie.Value = jwt.Token
 	cookie.HttpOnly = true
 	cookie.Expires = jwt.ExpAt     // we want the cookie to expire at the same time as the token
-	cookie.SameSite = 2            // Lax
+	cookie.SameSite = http.SameSiteLaxMode
 	cookie.Path = "/"              // Sen cookie in every sub path request
 	cookie.Secure = isProduction() // in production allow https only
 	c.SetCookie(cookie)
