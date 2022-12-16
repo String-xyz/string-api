2c859fcff11bab29106912db2d164422aaab020a
Auroter <auroter34@gmail.com>
2022-12-16 13:16:36 -0700

    Update entrypoint.sh to include new data seeding

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,20 @@
 export $(grep -v '^#' .env | xargs)
 
 # run db migrations
-echo "Running DB Migrations..."
-DB_CONFIG="host=$DB_HOST user=$DB_USERNAME dbname=$DB_NAME sslmode=disable password=$DB_PASSWORD"
-# goose postgres "$DB_CONFIG" reset # uncomment to reset db on build
+echo "----- Running migrations..."
 cd migrations
-goose postgres "$DB_CONFIG" up
-echo '>> Done!'
-# TODO: If migration fails then exit
+cd mocks
 
+DB_CONFIG="host=$DB_HOST user=$DB_USERNAME dbname=$DB_NAME sslmode=disable password=$DB_PASSWORD"
+goose postgres "$DB_CONFIG" reset
 cd ..
+goose postgres "$DB_CONFIG" reset
+goose postgres "$DB_CONFIG" up
+cd ..
+echo "----- ...Migrations done"
+echo "----- Seeding data..."
+go run scripts/data_seeding.go local
+echo "----- ...Data seeded"
+
+# run app
 air
