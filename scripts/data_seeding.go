50351d4e503cd2e6a75c786192c7ea7a3de90a38
Auroter <7332587+Auroter@users.noreply.github.com>
2022-12-20 13:20:42 -0700

    Update scripts/data_seeding.go
    Co-authored-by: akfoster <andrin@string.xyz>

--- scripts/data_seeding.go
+++ scripts/data_seeding.go
@@ -242,10 +242,6 @@ func mockSeeding() {
 
 	// Devices, this is used in TX LEG
 	/*deviceDeveloper*/
-	// _, err = repos.Device.Create(model.Device{LastUsedAt: time.Now(), Type: "Admin", Description: "Developer Laptop", UserID: userString.ID})
-	// if err != nil {
-	// 	panic(err)
-	// }
 
 	// Instruments, used in TX Legs
 	/*instrumentDeveloperCard*/
