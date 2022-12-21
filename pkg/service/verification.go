09498f6adc7e27429735267b2b6a484ce20ec401
Marlon Monroy <saito.sv.h@gmail.com>
2022-12-21 08:43:25 -0800

    wrong link

--- pkg/service/verification.go
+++ pkg/service/verification.go
@@ -120,7 +120,7 @@ func (v verification) SendDeviceVerification(userID, deviceID, deviceDescription
 	from := mail.NewEmail("String XYZ", "auth@string.xyz")
 	subject := "New Device Login Verification"
 	to := mail.NewEmail("New Device Login", email.Data)
-	link := baseURL + "verification?type=email&token=" + code
+	link := baseURL + "verification?type=device&token=" + code
 	htmlContent := fmt.Sprintf(`<div style="font-family: inherit; text-align: inherit"><span style="color: #172b4d; font-family: -apple-system, &quot;system-ui&quot;, &quot;Segoe UI&quot;, Roboto, Oxygen, Ubuntu, &quot;Fira Sans&quot;, &quot;Droid Sans&quot;, &quot;Helvetica Neue&quot;, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: -0.07px; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: pre-wrap; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; float: none; display: inline; font-size: 14px">We noticed that you attempted to log in from a new device %s. Is this you?</span></div>
 	<td align="center" bgcolor="#b7c23e" class="inner-td" style="border-radius:6px; font-size:16px; text-align:center; background-color:inherit;"><a href="%s" style="background-color:#b7c23e; border:1px solid 0; border-color:0; border-radius:6px; border-width:1px; color:#ffffff; display:inline-block; font-size:14px; font-weight:normal; letter-spacing:0px; line-height:normal; padding:12px 18px 12px 18px; text-align:center; text-decoration:none; border-style:solid;" target="_blank">Yes</a></td>`,
 		deviceDescription, link)
