191b401139295652b9ca7ca1ffb4feb7c39c1174
Marlon Monroy <saito.sv.h@gmail.com>
2023-01-06 13:19:09 -0800

    use servie name

--- pkg/store/pg.go
+++ pkg/store/pg.go
@@ -8,7 +8,6 @@ import (
 	"github.com/lib/pq"
 	sqltrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/database/sql"
 	sqlxtrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/jmoiron/sqlx"
-	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 var pgDB *sqlx.DB
@@ -47,7 +46,7 @@ func MustNewPG() *sqlx.DB {
 	if pgDB != nil {
 		return pgDB
 	}
-	sqltrace.Register(DBDriver, &pq.Driver{}, sqltrace.WithDBMPropagation(tracer.DBMPropagationModeFull))
+	sqltrace.Register(DBDriver, &pq.Driver{}, sqltrace.WithServiceName("string-api"))
 	connection, err := sqlxtrace.Open(DBDriver, strConnection())
 	if err != nil {
 		panic(err)
