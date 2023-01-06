e3b087335321ea88f98d869ca009024a8d5d6dad
Sean <sean@string.xyz>
2023-01-06 14:40:32 -0700

    Fixed a typo when sanitizing error stack trace

--- api/handler/common.go
+++ api/handler/common.go
@@ -37,8 +37,8 @@ func LogStringError(c echo.Context, err error, handlerMsg string) {
 	cause := errors.Cause(err)
 	st := tracer.StackTrace()
 
-	if os.Getenv("env") == "local" {
-		st2 := fmt.Sprintf("\n%+v: [%+v ]\n\n", cause.Error(), st[0:3])
+	if os.Getenv("ENV") == "local" {
+		st2 := fmt.Sprintf("\nSTACK TRACE:\n%+v: [%+v ]\n\n", cause.Error(), st[1:5])
 		// delete the string_api docker path from the stack trace
 		st2 = strings.ReplaceAll(st2, "/string_api/", "")
 		fmt.Print(st2)
