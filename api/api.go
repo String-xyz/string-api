1018e8b1f07596b0276d5b0700e841195ae116c8
Sean <sean@string.xyz>
2023-01-09 13:51:09 -0700

    re-enable API key protection

--- api/api.go
+++ api/api.go
@@ -40,7 +40,7 @@ func Start(config APIConfig) {
 	services := NewServices(config, repos)
 
 	// initialize routes - A route group only needs access to the services layer. It should'n access the repos layer directly
-	AuthAPIKey(services, e, true)
+	AuthAPIKey(services, e, false)
 	transactRoute(services, e)
 	quoteRoute(services, e)
 	userRoute(services, e)
