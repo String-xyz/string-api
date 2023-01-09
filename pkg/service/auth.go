65ccdecf90b32212c7f2f64a77448415607d9368
Sean <sean@string.xyz>
2023-01-09 13:57:43 -0700

    added a space between message and nonce

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -22,7 +22,7 @@ type SignablePayload struct {
 
 var hexRegex *regexp.Regexp = regexp.MustCompile(`^0x[a-fA-F0-9]{40}$`)
 
-var walletAuthenticationPrefix string = "Thank you for using String! By signing this message you are: 1) Authorizing String to initiate off-chain transactions on your behalf, including your bank account, credit card, or debit card. 2) Confirming that this wallet is owned by you. This request will not trigger any blockchain transaction or cost any gas:"
+var walletAuthenticationPrefix string = "Thank you for using String! By signing this message you are: 1) Authorizing String to initiate off-chain transactions on your behalf, including your bank account, credit card, or debit card. 2) Confirming that this wallet is owned by you. This request will not trigger any blockchain transaction or cost any gas: "
 
 type RefreshTokenResponse struct {
 	Token string    `json:"token"`
