a113ec39a23ccbb92596489016da459ba2b1ab0a
Auroter <7332587+Auroter@users.noreply.github.com>
2023-01-27 12:26:31 -0700

    Update pkg/service/platform.go
    Co-authored-by: akfoster <andrin@string.xyz>

--- pkg/service/platform.go
+++ pkg/service/platform.go
@@ -32,7 +32,6 @@ func (a platform) Create(c CreatePlatform) (model.Platform, error) {
 
 	_, err = a.repos.Auth.CreateAPIKey(plat.ID, c.Authentication, hashed, false)
 	pt := &plat
-	// pt.ApiKey = uuiKey
 	if err != nil {
 		return *pt, common.StringError(err)
 	}
