d8e21117a9bc619393ca60e51e282d3371c88bce
Sean <sean@string.xyz>
2023-02-02 11:53:45 -0700

    moved comma

--- migrations/0006_platform-member_member-to-platform_member-role_member-to-role_member-invite_apikey.sql
+++ migrations/0006_platform-member_member-to-platform_member-role_member-to-role_member-invite_apikey.sql
@@ -9,7 +9,7 @@ CREATE TABLE platform_member (
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   deactivated_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
   email TEXT NOT NULL,
-  password TEXT DEFAULT, '' -- how do we maintain this?
+  password TEXT DEFAULT '', -- how do we maintain this?
   name TEXT DEFAULT ''
 );
 
