ca784b29c364419287b9435883de9eeeb5acf5c4
Ocasta <ocasta181@gmail.com>
2023-02-24 18:48:34 -0600

    change to 'Fiat to Crypto'

--- pkg/internal/unit21/transaction.go
+++ pkg/internal/unit21/transaction.go
@@ -226,7 +226,7 @@ func mapToUnit21TransactionEvent(transaction model.Transaction, transactionData 
 			EventId:      transaction.ID,                    //required
 			EventType:    "transaction",                     //required
 			EventTime:    int(transaction.CreatedAt.Unix()), //required
-			EventSubtype: "Card Payment",                    //required for RTR
+			EventSubtype: "Fiat to Crypto",                  //required for RTR
 			Status:       transaction.Status,
 			Parents:      nil,
 			Tags:         transactionTagArr,
