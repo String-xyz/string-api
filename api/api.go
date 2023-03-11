0b0a5ba630bc3fbed70dd462e644ff4bc8dfba85
Wilfredo Alcala <alcalawilfre@gmail.com>
2023-03-11 14:12:46 -0500

    common middleware

--- api/api.go
+++ api/api.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/String-xyz/go-lib/common"
 	"github.com/String-xyz/go-lib/database"
+	"github.com/String-xyz/go-lib/middleware"
 	validator "github.com/String-xyz/go-lib/validator"
 	"github.com/String-xyz/string-api/api/handler"
-	"github.com/String-xyz/string-api/api/middleware"
+	_middleware "github.com/String-xyz/string-api/api/middleware"
+
 	"github.com/String-xyz/string-api/pkg/service"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
@@ -32,7 +34,7 @@ func Start(config APIConfig) {
 
 	// not internal middlewares
 	geofencingService := service.NewGeofencing(config.Redis)
-	e.Use(middleware.Georestrict(geofencingService))
+	e.Use(_middleware.Georestrict(geofencingService))
 
 	e.GET("/heartbeat", heartbeat)
 
@@ -70,7 +72,7 @@ func StartInternal(config APIConfig) {
 func baseMiddleware(logger *zerolog.Logger, e *echo.Echo) {
 	e.Use(middleware.Tracer())
 	e.Use(middleware.CORS())
-	e.Use(middleware.RequestId())
+	e.Use(middleware.RequestID())
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger(logger))
 	e.Use(middleware.LogRequest())
@@ -78,7 +80,7 @@ func baseMiddleware(logger *zerolog.Logger, e *echo.Echo) {
 
 func platformRoute(services service.Services, e *echo.Echo) {
 	handler := handler.NewPlatform(services.Platform)
-	handler.RegisterRoutes(e.Group("/platforms"), middleware.BearerAuth())
+	handler.RegisterRoutes(e.Group("/platforms"), _middleware.BearerAuth())
 }
 
 func AuthAPIKey(services service.Services, e *echo.Echo, internal bool) {
@@ -88,17 +90,17 @@ func AuthAPIKey(services service.Services, e *echo.Echo, internal bool) {
 
 func transactRoute(services service.Services, e *echo.Echo) {
 	handler := handler.NewTransaction(e, services.Transaction)
-	handler.RegisterRoutes(e.Group("/transactions"), middleware.APIKeyAuth(services.Auth), middleware.BearerAuth())
+	handler.RegisterRoutes(e.Group("/transactions"), _middleware.APIKeyAuth(services.Auth), _middleware.BearerAuth())
 }
 
 func userRoute(services service.Services, e *echo.Echo) {
 	handler := handler.NewUser(e, services.User, services.Verification)
-	handler.RegisterRoutes(e.Group("/users"), middleware.APIKeyAuth(services.Auth), middleware.BearerAuth())
+	handler.RegisterRoutes(e.Group("/users"), _middleware.APIKeyAuth(services.Auth), _middleware.BearerAuth())
 }
 
 func loginRoute(services service.Services, e *echo.Echo) {
 	handler := handler.NewLogin(e, services.Auth, services.Device)
-	handler.RegisterRoutes(e.Group("/login"), middleware.APIKeyAuth(services.Auth))
+	handler.RegisterRoutes(e.Group("/login"), _middleware.APIKeyAuth(services.Auth))
 }
 
 func verificationRoute(services service.Services, e *echo.Echo) {
@@ -108,5 +110,5 @@ func verificationRoute(services service.Services, e *echo.Echo) {
 
 func quoteRoute(services service.Services, e *echo.Echo) {
 	handler := handler.NewQuote(e, services.Transaction)
-	handler.RegisterRoutes(e.Group("/quotes"), middleware.APIKeyAuth(services.Auth), middleware.BearerAuth())
+	handler.RegisterRoutes(e.Group("/quotes"), _middleware.APIKeyAuth(services.Auth), _middleware.BearerAuth())
 }
--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -12,63 +12,7 @@ import (
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
 	"github.com/pkg/errors"
-	"github.com/rs/zerolog"
-	echoDatadog "gopkg.in/DataDog/dd-trace-go.v1/contrib/labstack/echo.v4"
 )
-
-func CORS() echo.MiddlewareFunc {
-	return echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-		AllowCredentials: true, // allow cookie auth
-	})
-}
-
-func Recover() echo.MiddlewareFunc {
-	return echoMiddleware.Recover()
-}
-
-func Logger(logger *zerolog.Logger) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			c.Set("logger", logger)
-			return next(c)
-		}
-	}
-}
-
-func LogRequest() echo.MiddlewareFunc {
-	return echoMiddleware.RequestLoggerWithConfig(echoMiddleware.RequestLoggerConfig{
-		LogURI:       true,
-		LogStatus:    true,
-		LogRequestID: true,
-		LogLatency:   true,
-		LogMethod:    true,
-		LogHost:      true,
-		LogError:     true,
-		LogValuesFunc: func(c echo.Context, v echoMiddleware.RequestLoggerValues) error {
-			env := os.Getenv("ENV")
-			logger := c.Get("logger").(*zerolog.Logger)
-			logger.Info().
-				Str("path", v.URI).
-				Str("method", v.Method).
-				Int("status_code", v.Status).
-				Str("request_id", v.RequestID).
-				Str("host", v.Host).
-				Dur("latency", v.Latency).
-				Str("env", env).
-				Err(v.Error).
-				Msg("request")
-
-			return nil
-		},
-	})
-}
-
-// RequestID generates a unique request ID
-func RequestId() echo.MiddlewareFunc {
-	return echoMiddleware.RequestID()
-}
 
 func BearerAuth() echo.MiddlewareFunc {
 	config := echoMiddleware.JWTConfig{
@@ -108,10 +52,6 @@ func APIKeyAuth(service service.Auth) echo.MiddlewareFunc {
 		},
 	}
 	return echoMiddleware.KeyAuthWithConfig(config)
-}
-
-func Tracer() echo.MiddlewareFunc {
-	return echoDatadog.Middleware()
 }
 
 func Georestrict(service service.Geofencing) echo.MiddlewareFunc {
