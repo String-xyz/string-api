b2c3e562f67ac98f48b5a7ddb00e7489f7f35556
Ocasta <ocasta181@gmail.com>
2023-04-11 12:41:28 -0700

    update iam roles content on dev and order on sandbox

--- infra/dev/iam_roles.tf
+++ infra/dev/iam_roles.tf
@@ -56,7 +56,8 @@ data "aws_iam_policy_document" "task_policy" {
       data.aws_ssm_parameter.sendgrid_api_key.arn,
       data.aws_ssm_parameter.twilio_sms_sid.arn,
       data.aws_ssm_parameter.twilio_account_sid.arn,
-      data.aws_ssm_parameter.twilio_auth_token.arn
+      data.aws_ssm_parameter.twilio_auth_token.arn,
+      data.aws_ssm_parameter.team_phone_numbers.arn
     ]
   }
 
