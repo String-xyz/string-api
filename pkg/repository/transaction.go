5100b24da9eb1a0633ac4c343d589ed75ecab3b9
alcalawil <alcalawilfre@gmail.com>
2023-04-17 13:40:16 -0300

    context

--- pkg/repository/transaction.go
+++ pkg/repository/transaction.go
@@ -11,7 +11,7 @@ import (
 
 type Transaction interface {
 	database.Transactable
-	Create(model.Transaction) (model.Transaction, error)
+	Create(ctx context.Context, tx model.Transaction) (model.Transaction, error)
 	GetById(ctx context.Context, id string) (model.Transaction, error)
 	Update(ctx context.Context, id string, updates any) error
 }
@@ -24,22 +24,22 @@ func NewTransaction(db database.Queryable) Transaction {
 	return &transaction[model.Transaction]{baserepo.Base[model.Transaction]{Store: db, Table: "transaction"}}
 }
 
-func (t transaction[T]) Create(insert model.Transaction) (model.Transaction, error) {
+func (t transaction[T]) Create(ctx context.Context, insert model.Transaction) (model.Transaction, error) {
 	m := model.Transaction{}
 	// TODO: Add platform_id once it becomes available
-	rows, err := t.Store.NamedQuery(`
+	query := `
 		INSERT INTO transaction (status, network_id, device_id, platform_id, ip_address)
-		VALUES(:status, :network_id, :device_id, :platform_id, :ip_address) RETURNING id`, insert)
+		VALUES(:status, :network_id, :device_id, :platform_id, :ip_address) RETURNING id`
+	stmt, err := t.Store.PrepareContext(ctx, query)
 	if err != nil {
 		return m, libcommon.StringError(err)
 	}
-	for rows.Next() {
-		err = rows.Scan(&m.Id)
-		if err != nil {
-			return m, libcommon.StringError(err)
-		}
+	defer stmt.Close()
+
+	err = stmt.QueryRowContext(ctx, insert).Scan(&m.Id)
+	if err != nil {
+		return m, libcommon.StringError(err)
 	}
 
-	defer rows.Close()
 	return m, nil
 }
