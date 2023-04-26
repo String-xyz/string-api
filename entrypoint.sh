6ef28a35bc8ed03119a15440f0dbd5958b57b1a6
Sean <sean@string.xyz>
2023-04-26 11:02:48 -0700

    manually undo git clobber

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,18 @@
 # export env variables from .env file
 export $(grep -v '^#' .env | xargs)
 
-if [ "$DB_RESET" = "true" ]; then
-  # run db migrations
-  echo "----- Running migrations..."
-  cd migrations
+# run db migrations
+echo "----- Running migrations..."
+cd migrations
 
-  DB_CONFIG="host=$DB_HOST user=$DB_USERNAME dbname=$DB_NAME sslmode=disable password=$DB_PASSWORD"
-  goose postgres "$DB_CONFIG" reset
-  goose postgres "$DB_CONFIG" up
-  cd ..
-  echo "----- ...Migrations done"
-  echo "----- Seeding data..."
-  go run script.go data_seeding local
-  echo "----- ...Data seeded"
-fi
+DB_CONFIG="host=$DB_HOST user=$DB_USERNAME dbname=$DB_NAME sslmode=disable password=$DB_PASSWORD"
+goose postgres "$DB_CONFIG" reset
+goose postgres "$DB_CONFIG" up
+cd ..
+echo "----- ...Migrations done"
+echo "----- Seeding data..."
+go run script.go data_seeding local
+echo "----- ...Data seeded"
 
 # run app
 if [ "$DEBUG_MODE" = "true" ]; then
