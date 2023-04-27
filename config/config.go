1247362b1fb28508205e8968459d7dfd4e316edf
Auroter <7332587+Auroter@users.noreply.github.com>
2023-04-27 11:12:07 -0700

    If an env var is missing, panic and print all missing env vars (#184)

--- config/config.go
+++ config/config.go
@@ -1,9 +1,9 @@
 package config
 
 import (
-	"errors"
 	"os"
 	"reflect"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -62,15 +62,19 @@ var Var vars
 
 func LoadEnv() error {
 	godotenv.Load(".env")
+	missing := []string{}
 	stype := reflect.ValueOf(&Var).Elem()
 	for i := 0; i < stype.NumField(); i++ {
 		field := stype.Field(i)
 		key := stype.Type().Field(i).Name
 		value := os.Getenv(key)
 		if value == "" {
-			return errors.New("Missing environment variable: " + key)
+			missing = append(missing, key)
 		}
 		field.SetString(value)
+	}
+	if len(missing) > 0 {
+		panic("Missing environment variable: " + strings.Join(missing, ", "))
 	}
 	return nil
 }
