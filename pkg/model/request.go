9f4222e187051f0c948f91beae936a4d9783274c
Wilfredo Alcala <alcalawilfre@gmail.com>
2023-04-28 16:49:35 -0300

    fix UserUpdates type

--- pkg/model/request.go
+++ pkg/model/request.go
@@ -71,7 +71,7 @@ type UserUpdates struct {
 	Type       *string         `json:"type" db:"type"`
 	Status     *string         `json:"status" db:"status"`
 	Tags       *types.JSONText `json:"tags" db:"tags"`
-	FirstNname *string         `json:"firstName" db:"first_name"`
+	FirstName  *string         `json:"firstName" db:"first_name"`
 	MiddleName *string         `json:"middleName" db:"middle_name"`
 	LastName   *string         `json:"lastName" db:"last_name"`
 }
