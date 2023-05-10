8bcc8e5d2039361ffe9b3b60b3641d6d9ec38f3a
Ocasta <ocasta181@gmail.com>
2023-05-10 15:47:22 -0700

    ensure every value in TransactionRequest is required

--- pkg/model/transaction.go
+++ pkg/model/transaction.go
@@ -38,12 +38,12 @@ type PaymentInfo struct {
 type TransactionRequest struct {
 	UserAddress string   `json:"userAddress" validate:"required,eth_addr"`     // Used to keep track of user ie "0x44A4b9E2A69d86BA382a511f845CbF2E31286770"
 	AssetName   string   `json:"assetName" validate:"required,min=3,max=30"`   // Used for receipt
-	ChainId     uint64   `json:"chainId"`                                      // Chain ID to execute on e.g. 80000. // TODO: keep a list of supported chains and validate against that
+	ChainId     uint64   `json:"chainId" validate:"required,number"`           // Chain ID to execute on e.g. 80000.
 	CxAddr      string   `json:"contractAddress" validate:"required,eth_addr"` // Address of contract ie "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-	CxFunc      string   `json:"contractFunction"`                             // Function declaration ie "mintTo(address) payable"
-	CxReturn    string   `json:"contractReturn"`                               // Function return type ie "uint256"
-	CxParams    []string `json:"contractParameters"`                           // Function parameters ie ["0x000000000000000000BEEF", "32"]
-	TxValue     string   `json:"txValue"`                                      // Amount of native token to send ie "0.08 ether"
+	CxFunc      string   `json:"contractFunction" validate:"required,string"`  // Function declaration ie "mintTo(address)"
+	CxReturn    string   `json:"contractReturn" validate:"required,string"`    // Function return type ie "uint256"
+	CxParams    []string `json:"contractParameters" validate:"required"`       // Function parameters ie ["0x000000000000000000BEEF", "32"]
+	TxValue     string   `json:"txValue" validate:"required,string"`           // Amount of native token to send ie "0.08 ether"
 	TxGasLimit  string   `json:"gasLimit" validate:"required,number"`          // Gwei gas limit ie "210000 gwei"
 }
 
