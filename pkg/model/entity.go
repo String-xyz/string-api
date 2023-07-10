5daf82cf8ec6a1f147309ee3deeaa31195f42b78
saito-sv <7920256+saito-sv@users.noreply.github.com>
2023-07-09 22:04:49 -0700

    added contract to platform (#218)
    * added contract to platform
    
    * fix sql call
    
    ---------
    
    Co-authored-by: Ocasta <ocasta181@gmail.com>

--- pkg/model/entity.go
+++ pkg/model/entity.go
@@ -227,16 +227,16 @@ type Apikey struct {
 }
 
 type Contract struct {
-	Id            string         `json:"id,omitempty" db:"id"`
-	CreatedAt     time.Time      `json:"createdAt,omitempty" db:"created_at"`
-	UpdatedAt     time.Time      `json:"updatedAt,omitempty" db:"updated_at"`
-	DeactivatedAt *time.Time     `json:"deactivatedAt,omitempty" db:"deactivated_at"`
-	DeletedAt     *time.Time     `json:"deletedAt,omitempty" db:"deleted_at"`
-	Name          string         `json:"name" db:"name"`
-	Address       string         `json:"address" db:"address"`
-	Functions     pq.StringArray `json:"functions" db:"functions"`
-	NetworkId     string         `json:"networkId" db:"network_id"`
-	PlatformId    string         `json:"platformId" db:"platform_id"`
+	Id             string         `json:"id,omitempty" db:"id"`
+	CreatedAt      time.Time      `json:"createdAt,omitempty" db:"created_at"`
+	UpdatedAt      time.Time      `json:"updatedAt,omitempty" db:"updated_at"`
+	DeactivatedAt  *time.Time     `json:"deactivatedAt,omitempty" db:"deactivated_at"`
+	DeletedAt      *time.Time     `json:"deletedAt,omitempty" db:"deleted_at"`
+	Name           string         `json:"name" db:"name"`
+	Address        string         `json:"address" db:"address"`
+	Functions      pq.StringArray `json:"functions" db:"functions" swaggertype:"array,string"`
+	NetworkId      string         `json:"networkId" db:"network_id"`
+	OrganizationId string         `json:"organizationId" db:"organization_id"`
 }
 
 func (a AuthStrategy) MarshalBinary() ([]byte, error) {
