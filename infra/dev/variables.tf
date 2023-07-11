ef2d93dba4a15276ac22de52a20ade59c61aada5
saito-sv <7920256+saito-sv@users.noreply.github.com>
2023-07-11 15:14:39 -0700

    docker tags and service name (#222)
    * docker tags and service name
    
    * docker tags

--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -26,7 +26,11 @@ locals {
       essential = true,
       dockerLabels = {
         "com.datadoghq.ad.instances" : "[{\"host\":\"%%host%%\"}]",
-        "com.datadoghq.ad.check_names" : "[\"${local.service_name}\"]",
+        "com.datadoghq.ad.logs" : "[{\"service\":\"${local.service_name}\"}]",
+        "com.datadoghq.ad.check_names" :local.service_name,
+        "com.datadoghq.tags.env": local.env,
+        "com.datadoghq.tags.service": local.service_name,
+        "com.datadoghq.tags.version": var.versioning
       },
       portMappings = [
         {
