a11adec2e1ca4ccbf6539ad61458642c54c08c4c
Sean <sean@string.xyz>
2023-07-16 11:57:28 -0700

    Fix another merge issue

--- pkg/service/cost_test.go
+++ pkg/service/cost_test.go
@@ -9,7 +9,7 @@ import (
 func TestGetTokenPrices(t *testing.T) {
 	keyMap, err := GetCoingeckoCoinMapping()
 	assert.NoError(t, err)
-	name, ok := keyMap[CoinKey{ChainId: 43114, Address: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e"}]
+	name, ok := keyMap[CoinKey{ChainId: 43114, Address: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e"}.String()]
 	assert.True(t, ok)
 	assert.Equal(t, "usd-coin", name)
 }
