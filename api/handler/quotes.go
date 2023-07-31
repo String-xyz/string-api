9430a557698c85f78a420401f9403364bb3126cb
Sean <sean@string.xyz>
2023-07-31 13:31:02 -0700

    ensure unsigned quote payload is returned with 403 if KYC level is not sufficient

--- api/handler/quotes.go
+++ api/handler/quotes.go
@@ -70,6 +70,11 @@ func (q quote) Quote(c echo.Context) error {
 	}
 
 	res, err := q.Service.Quote(ctx, body, platformId)
+
+	if errors.Cause(err).Error() == "insufficient level" { // TODO: use a custom error
+		return c.JSON(http.StatusForbidden, res)
+	}
+
 	if err != nil {
 		libcommon.LogStringError(c, err, "quote: quote")
 
